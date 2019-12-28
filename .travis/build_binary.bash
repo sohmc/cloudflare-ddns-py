9eac4adffd451d969c1ef35d62ba12bd9599c57e
Michael Soh <sohmc@users.noreply.github.com>
2019-12-28 17:37:05 -0500

    Wrong option for s3 cp

--- .travis/build_binary.bash
+++ .travis/build_binary.bash
@@ -7,4 +7,4 @@ pyinstaller --onefile ./cloudflare-ddns.py
 export BIN_NAME=cloudflare-ddns-${TRAVIS_OS_NAME}-${TRAVIS_CPU_ARCH}.bin
 cp ./dist/cloudflare-ddns ./dist/${BIN_NAME}
 ls -lR
-aws s3 cp ./dist/${BIN_NAME} s3://${AWS_TRAVIS_DEPLOY_BUCKET}/cloudflare_ddns/${BUILD_ID}/ --dry-run
+aws s3 cp ./dist/${BIN_NAME} s3://${AWS_TRAVIS_DEPLOY_BUCKET}/cloudflare_ddns/${BUILD_ID}/ --dryrun

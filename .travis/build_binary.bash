477f1a28f170192b4ef797455cab350bea03180f
Michael Soh <sohmc@users.noreply.github.com>
2019-12-28 15:48:42 -0500

    Moved to script

---
+++ .travis/build_binary.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -ev
+pip install awscli
+pip install pyinstaller
+pyinstaller --onefile ./cloudflare-ddns.py
+export BIN_NAME=cloudflare-ddns-${TRAVIS_OS_NAME}-${TRAVIS_CPU_ARCH}.bin
+cp ./dist/cloudflare-ddns ./dist/${BIN_NAME}
+ls -lR
+aws s3 cp ./dist/${BIN_NAME} s3://${AWS_TRAVIS_DEPLOY_BUCKET}/cloudflare_ddns/${BUILD_ID}/

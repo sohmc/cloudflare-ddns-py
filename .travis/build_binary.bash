0adf7c6366025d02c8e7416be464c6f595c8fdbd
Michael Soh <sohmc@users.noreply.github.com>
2020-11-11 18:02:26 -0500

    Debugging aws install on windows

--- .travis/build_binary.bash
+++ .travis/build_binary.bash
@@ -25,11 +25,10 @@ elif [[ $TRAVIS_OS_NAME == "osx" ]]; then
     sudo installer -pkg awscliv2.pkg -target /
 elif [[ $TRAVIS_OS_NAME == "windows" ]]; then
     echo "Installing via choco..."
-    choco install awscli
+#    choco install awscli
 fi
 
 echo "Testing for AWS cli"
-
 aws --version
 
 

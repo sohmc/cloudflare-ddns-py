ed6aa5e7d22781f822f04e3a0d9b75956bb12a96
Michael Soh <2767724+sohmc@users.noreply.github.com>
2021-01-24 17:35:36 -0500

    Test Binaries

--- .travis/test_binary.bash
+++ .travis/test_binary.bash
@@ -1,16 +1,16 @@
 #!/bin/bash
 BIN_NAME=cloudflare-ddns-${TRAVIS_OS_NAME}-${TRAVIS_CPU_ARCH}.bin
 
-if [[ $TRAVIS_OS_NAME == "windows" ]]; then
-    BIN_NAME=cloudflare-ddns-${TRAVIS_OS_NAME}-${TRAVIS_CPU_ARCH}.exe
-fi
+# if [[ $TRAVIS_OS_NAME == "windows" ]]; then
+#     BIN_NAME=cloudflare-ddns-${TRAVIS_OS_NAME}-${TRAVIS_CPU_ARCH}.exe
+# fi
 
-S3_URI=https://s3.amazonaws.com/${AWS_TRAVIS_DEPLOY_BUCKET}/cloudflare_ddns/${BUILD_ID}/${BIN_NAME}
+# S3_URI=https://s3.amazonaws.com/${AWS_TRAVIS_DEPLOY_BUCKET}/cloudflare_ddns/${BUILD_ID}/${BIN_NAME}
 
 set -ev
 
-echo "Getting binary from S3..."
-curl -v $S3_URI -o $BIN_NAME
+# echo "Getting binary from S3..."
+# curl -v $S3_URI -o $BIN_NAME
 
 chmod +x ${BIN_NAME}
 ls -lR
